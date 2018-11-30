53a62116949ea962d4361440ecaada7ed55ec843
Nobutaka Oshiro <n_ohshiro@yumemi.co.jp>
2018-11-30 12:09:04 +0900

    Add NativeBase & Prettier packages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Container, Text } from 'native-base'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Text> Hello, World! </Text>
+      </Container>
+    )
+  }
+}
