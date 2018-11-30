dd48d056cd167f2f55e8ec8bbebd9b219a4ae597
Nobutaka Oshiro <n_ohshiro@yumemi.co.jp>
2018-11-30 12:32:44 +0900

    Update prettier config

---
+++ README.md
@@ -0,0 +1,16 @@
+# HackathonApp 20181130
+
+# How to build
+
+* Requirements
+    * Xcode 10.x & Xcode Commandline Tools
+    * node >= v10.3.0
+    * npm >= 6.4.1
+    * yarn
+
+```bash
+git clone https://github.com/yumemi-nohshiro/HackathonAppProject20181130.git
+cd HackathonAppProject20181130
+yarn install
+yarn run dev:ios
+```
--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
-import { Container, Text } from 'native-base'
+import React, { Component } from "react"
+import { Container, Header, Content, Footer, Text, Button } from "native-base"
+
+function AppHeader() {
+  return <Header />
+}
+
+function AppFooter() {
+  return <Footer />
+}
+
+const AppContent = () => (
+  <Content>
+    <Text> Hello, World!</Text>
+    <Text> Hello, World!</Text>
+    <Text> Hello, World!</Text>
+    <Text> Hello, World!</Text>
+    <Text> Hello, World!</Text>
+    <Text> Hello, World!</Text>
+    <Button><Text>Hello!</Text></Button>
+  </Content>
+)
 
 export default class App extends Component {
   render() {
     return (
       <Container>
-        <Text> Hello, World! </Text>
+        <AppHeader />
+        <AppContent />
+        <AppFooter />
       </Container>
     )
   }
